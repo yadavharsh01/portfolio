690a650665e837d82c43fdaf28493d025fa477bf
Harsh Yaduvanshi <harshyadav.hy20@gmail.com>
2024-02-18 14:55:25 +0530

    added projects, skills sections with some animation effects

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".text", {
+  strings: ["Web Developer","YouTuber","MERN Stack Dev","Cyber Security Enthusiast"],
+  typeSpeed:100,
+  backSpeed:60,
+  backDelay:1000,
+  loop: true,
+})
