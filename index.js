74cbed1e507f688295bd644ae144459008b20dc2
Harsh Yaduvanshi <harshyadav.hy20@gmail.com>
2024-02-20 04:09:15 +0530

    some final changes and tailored layout

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-  strings: ["Web Developer","YouTuber","MERN Stack Dev","Cyber Security Enthusiast"],
+  strings: ["Web Developer","MERN Stack Dev","YouTuber","Cyber Security Enthusiast"],
   typeSpeed:100,
   backSpeed:60,
   backDelay:1000,
